e61b9addaa92c831f6b2c753545a704d9ff702e4
Artem <artem-tikhonov-90@list.ru>
2022-08-30 13:11:21 +0300

    new commit

--- TasksProject/AppDelegate.swift
+++ TasksProject/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Override point for customization after application launc.
         return true
     }
 
@@ -27,7 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWitOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
